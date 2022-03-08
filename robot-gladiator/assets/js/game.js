33f5ef0e79e89e7fd9c4c1c473d406447b44c9ee
velarde.brandon02@gmail.com <velarde.brandon02@gmail.com>
2022-03-07 21:23:27 -0600

    3.1.5 mid way at concatenates

--- robot-gladiator/assets/js/game.js
+++ robot-gladiator/assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("this is an alert! JavaScript is running!")
+
+var playerName= window.prompt("What is your robot's name?")
+ 
+ console.log(playerName);
+ console.log("this logs a string, good for laving yourslef a message");
+ console.log(10+10);
+ console.log("Our robot's name is " + playerName);
+
+
+function fight (){
+    window.prompt("the fight has begun")
+}
+
+
+
+//fight();
