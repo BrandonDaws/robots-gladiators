bf688ba4adcc8eb1044fb30c00803ad98242ad66
velarde.brandon02@gmail.com <velarde.brandon02@gmail.com>
2022-03-08 18:59:35 -0600

    3.1.6 added notes for next steps

--- robot-gladiator/assets/js/game.js
+++ robot-gladiator/assets/js/game.js
@@ -1,16 +1,24 @@
 
 var playerName= window.prompt("What is your robot's name?")
- 
- console.log(playerName);
- console.log("this logs a string, good for laving yourslef a message");
- console.log(10+10);
- console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "DOMO";
+var enmyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    //alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators");
+    //subtract the value of "player attack" from the value of the "enemy health" and use that result to update the value in the enemy health
+    
+    //log a resultingmessage to the console so we know that it worked. 
+
+    //subtract the value of the "enemy attack" from the value of "Playerhealth" and use that to update the value in the "playerhealth" variable.
+
+    //log a resulting message to the console so we know that it worked.
+};
 
 
-function fight (){
-    window.prompt("the fight has begun")
-}
 
-
-
-//fight();
+fight();
